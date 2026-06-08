58008277d70e45b29a6ac39b97dfb3fd2fb90dcc
Clementine Pendragon <clem@pendragon.systems>
2026-06-07 19:05:28 -0600

    clean up files

--- test_httpcore2.py
+++
@@ -1,5 +0,0 @@
-import httpx
-
-t = httpx.AsyncHTTPTransport()
-print(dir(t))
-print(type(t._pool))
--- test_httpcore3.py
+++
@@ -1,6 +0,0 @@
-import httpx
-
-t = httpx.AsyncHTTPTransport()
-print(dir(t._pool))
-if hasattr(t._pool, '_network_backend'):
-    print(t._pool._network_backend)
